67b1b29c38a24ae4b5c6f74cfa4a16896be4dc1d
marselakhmetov <makhmetov@provectus.com>
2022-07-15 16:00:43 +0300

    Adding paginating by date

--- src/main/java/ru/demo_bot_minecraft/action/ServerLogsAction.java
+++ src/main/java/ru/demo_bot_minecraft/action/ServerLogsAction.java
@@ -1,5 +1,7 @@
 package ru.demo_bot_minecraft.action;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -34,7 +36,7 @@ public class ServerLogsAction implements Action {
         SendMessage message;
         StringBuilder messageBuilder = new StringBuilder();
         messageBuilder.append("Logs: \n");
-        serverEventRepository.findAll()
+        serverEventRepository.findAllByTimeBetween(LocalDate.now().atStartOfDay(), LocalDateTime.now())
             .forEach(event -> messageBuilder.append(event.getTime().format(
                     DateTimeFormatter.ofPattern("dd.MM HH:mm"))).append(" ")
                 .append(event.getPlayer().getName()).append(" ").append(event.getAction())
--- src/main/java/ru/demo_bot_minecraft/repository/ServerEventRepository.java
+++ src/main/java/ru/demo_bot_minecraft/repository/ServerEventRepository.java
@@ -1,8 +1,12 @@
 package ru.demo_bot_minecraft.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.demo_bot_minecraft.domain.database.ServerEvent;
 
 public interface ServerEventRepository extends JpaRepository<ServerEvent, Long> {
+
+    List<ServerEvent> findAllByTimeBetween(LocalDateTime from, LocalDateTime to);
 
 }
